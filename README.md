a47ce796cb6a6c57b0aefe7f34870de65569a206
wonkyum-kim <dodo12120@naver.com>
2024-05-04 14:12:44 +0900

    feat: init

---
+++ README.md
@@ -0,0 +1,12 @@
+## rollup setting template
+
+기본적인 rollup 세팅
+
+ESM과 CJS 모두 출력한다.
+
+## TODO
+
+- [ ] package.json exports 추가
+- [ ] babel
+- [ ] terser
+- [ ] plugin commonjs
---
+++ rollup.config.js
@@ -0,0 +1,78 @@
+import path from 'path';
+import dts from 'rollup-plugin-dts';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+
+const extensions = ['.js', '.ts', '.tsx'];
+
+const external = [
+  /* ... */
+];
+
+function buildJS(input, output, module) {
+  return {
+    input,
+    output: {
+      file: output,
+      format: module,
+    },
+    external,
+    plugins: [
+      resolve({ extensions }),
+      typescript({
+        declaration: false,
+      }),
+    ],
+  };
+}
+
+// CJS 모듈 시스템을 사용하는 코드
+function buildCJS(input) {
+  const parsed = path.parse(input);
+  return buildJS(
+    `src/${input}`,
+    `dist/cjs/${parsed.dir}/${parsed.name}.cjs`,
+    'cjs'
+  );
+}
+
+// ES 모듈 시스템을 사용하는 코드
+function buildESM(input) {
+  const parsed = path.parse(input);
+  return buildJS(
+    `src/${input}`,
+    `dist/esm/${parsed.dir}/${parsed.name}.js`,
+    'esm'
+  );
+}
+
+function buildDTS(input, module) {
+  const parsed = path.parse(input);
+  const isESM = module === 'esm';
+  const dir = `dist/${isESM ? 'esm' : 'cjs'}/${parsed.dir}/`;
+  const ext = isESM ? '.ts' : '.cts';
+
+  return {
+    input: `./types/${input}`,
+    output: {
+      file: `${dir}${parsed.name}${ext}`,
+      format: module,
+    },
+    plugins: [dts()],
+  };
+}
+
+function buildCJSDTS(input) {
+  return buildDTS(input, 'cjs');
+}
+
+function buildESMDTS(input) {
+  return buildDTS(input, 'esm');
+}
+
+export default [
+  buildESM('index.ts'),
+  buildCJS('index.ts'),
+  buildESMDTS('index.d.ts'),
+  buildCJSDTS('index.d.ts'),
+];
